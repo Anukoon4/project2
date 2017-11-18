ce97ea9b97675ec392e12171add512e7ddc3e9db
aeasd0000 <anukoon.ae@gmail.com>
2017-11-18 15:44:08 +0700

    Update

---
+++ index.js
@@ -0,0 +1,37 @@
+
+const async = require('async')
+const fs = require('fs')
+const cheerio=require('cheerio')
+
+const item = [
+    {
+        name: 'taara',
+        url: 'http://rov.wikia.com/wiki/Taara'
+    },
+    {
+        name: 'Valhein',
+        url: 'http://rov.wikia.com/wiki/Valhein'
+    }
+]
+
+const queue = async.queue((task, callback) => {
+    request(task.url, (error, response, body) => {
+        $ = cheerio.load(body)
+        const text=$('mw-content-text p').text()
+       
+        fs.witeFire(task.name+ ".txt",text, (err) => {
+            if (err) {
+                console.log(err)
+                callback()
+            }
+            console.log("file saved")
+            callback()
+        })
+    })
+}, 1)
+
+queue.drain = () => {
+    console.log("All process complete")
+}
+
+queue.push(item)
